fcdb2f9382aec98c358f882ee1463416397f6e3a
qadroid <159qadroid951@gmail.com>
2023-05-11 15:37:02 +0100

    * feat(main.cpp): add alarm clock functionality with buzzer and buttons * feat(main.cpp): add display for temperature and time remaining to alarm * feat(main.cpp): add calculation for time remaining to alarm * feat(main.cpp): add buzzer functionality when alarm rings * refactor(main.cpp): change variable names for better readability * refactor(main.cpp): remove unused code and libraries

--- src/main.cpp
+++ src/main.cpp
@@ -4,57 +4,80 @@
 
 
 /**
- * This alarm clock will ring based on the time.
- * It will play a buzzer at said times.
+ * This is an alarm clock designed to ring after 8.5 hours of starting.
+ * It will play a buzzer at the end of it's timer.
  * A button will be used to turn it off.
- * Lights will play along with the buzzer.
- * A screen will show temperature and 'weather info?' but will change when the alarm is active 
+ * Planned to add lighting to the sound of buzzer.
+ * A screen will show temperature and time remaining to alarm.  
  */
 
 /**
- * A lot of this code is borrowed from example code from Elegoo's and Arduino's resources.
+ * A lot of this code is borrowed and modified from examples and other projects.
 */
 
-
+// ----------------------------
+// Imports and definitions
 #include "Arduino.h"
-#include <RTClib.h>
-#include <LiquidCrystal.h>
-#include "DHT_Async.h"
-#include <SPI.h>
-#define DHT_SENSOR_TYPE DHT_TYPE_11
+#include "RTClib.h"
+#include "Wire.h"
+#include "LiquidCrystal.h"
+#include "DHT_U.h"
+#include "SPI.h"
+#include "Tone.h"
+#include "IRremote.hpp"
+#include "AlarmTimer.h"
+#include "TempSensor.h"
+#include "LEDRow.h"
+#include "DHTReader.h"
+#define buzzer 6
+// ----------------------------
 
+// ----------------------------
+// Defining units of time as well as timer length
+int second = 1000;
+int minute = 60 * second;
+int hour = 60 * minute;
+int alarmTimeRemaining = 8.5 * hour;
+// ----------------------------
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
-
-// DHT setup
-static const int DHT_SENSOR_PIN = 0;
-DHT_Async dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
-
-
-// Alarm data
+// ----------------------------
+// Setting lock values to be switched later
 int alarmActive = 0;
-int alarmStopped = 0;
+int alarmRinging = 0;
+int alarmStopped = 1;
+// ----------------------------
 
+// ----------------------------
+// Alarm buttons
+int stopButton = 7;
+int resetButton = 2;
+// ----------------------------
 
-// Liquid Crystal components
+// ----------------------------
+// DHT sensor setup
+int DHT_SENSOR_PIN = 1;
+// ----------------------------
+
+// ----------------------------
+// IR sensor setup
+int IR_SENSOR_PIN = 0;
+// ----------------------------
+
+// ----------------------------
+// Liquid Crystal setup
 //                BS E  D4  D5  D6  D7
 LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+// ----------------------------
 
-// RTC components
-RTC_DS1307 RTC;
+// ----------------------------
+// Buzzer setup
+int melody[] = {
+  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+int duration = 500;  // 500 miliseconds
+// ----------------------------
 
-int hourupg;
-int minupg;
-int yearupg;
-int monthupg;
-int dayupg;
-int menu =0;
-int setAll =0;
-
-uint8_t alarmHours = 0, alarmMinutes = 0;  // Holds the current alarm time
-
-// LED row setup
+// ----------------------------
+// LED row setup (Not yet implemented)
 int tDelay = 100;
 int latchPin = 4;      // (4) ST_CP [RCK] on 74HC595
 int clockPin = 3;      // (3) SH_CP [SCK] on 74HC595
@@ -68,7 +91,53 @@ void updateShiftRegister()
    shiftOut(dataPin, clockPin, LSBFIRST, leds);
    digitalWrite(latchPin, HIGH);
 }
+// ----------------------------
 
+// The purpose of this function is to type out the early text and call the next functions for temp and timing.
+// ----------------------------
+void displayTimer()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:");
+  lcd.setCursor(5, 0);
+  // Implement temp sensor here later
+  // Enter code here:
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Time left:");
+  lcd.setCursor(10, 1);
+
+  // Calculating time remaining
+  char timeRemaining[] = "08:30";
+
+  // Calculating hours remaining
+  char hoursRemaining = alarmTimeRemaining / 60 * minute;
+
+  // Applying hours to result
+  if (alarmTimeRemaining >= 60 * minute) 
+  {
+    timeRemaining[1] = hoursRemaining;
+  } else {
+    timeRemaining[1] = '0';
+  }
+
+  // Calculating minutes remaining
+  int minutesRemaining = alarmTimeRemaining / 60 * second;
+
+  // Applying minutes to result
+  if (alarmTimeRemaining >= 10 * minute) 
+  {
+    timeRemaining[3,4] = minutesRemaining;
+  } else { 
+    timeRemaining[4] = minutesRemaining;
+  }
+
+  // Print the result
+  lcd.print(timeRemaining);
+}
+// ----------------------------
+
+// ----------------------------
 void setup()
 {
 
@@ -77,85 +146,64 @@ void setup()
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
 
+  // This part is for the buttons
+  pinMode(stopButton, INPUT_PULLUP);
+  pinMode(resetButton, INPUT_PULLUP);
+
   // From the clock project
   Serial.begin(9600);
   Wire.begin();
-  RTC.begin();
 
-  if (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // Set the date and time at compile time
-    RTC.adjust(DateTime(__DATE__, __TIME__));
-  }
-  // RTC.adjust(DateTime(__DATE__, __TIME__)); //removing "//" to adjust the time
-    // The default display shows the date and time
-  int menu=0;
-
-  
+  // Initialize lcd 
+  lcd.begin(16, 2);
 }
+// ----------------------------
 
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
+// ----------------------------
+void loop() 
 {
-  static unsigned long measurement_timestamp = millis( );
-
-  /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
+  displayTemp();
+  displayTempTimer();
+  delay(1000);
+  if (digitalRead(resetButton) == LOW) 
   {
-    if( dht_sensor.measure( temperature, humidity ) == true )
+    if (alarmActive = 0)
     {
-      measurement_timestamp = millis( );
-      return( true );
+      alarmActive = 1;
+      alarmStopped = 0;
+    } else {
+      alarmActive = 0;
+      alarmTimeRemaining = 8.5 * hour;
     }
   }
 
-  return( false );
-}
-
-
-void loop()
-{
-  // Eight LEDs
-  leds = 0;
-  updateShiftRegister();
-  delay(tDelay);
-  for (int i = 0; i < 6; i++)
+  while (alarmActive == 1 && alarmTimeRemaining > 0) 
   {
-    bitSet(leds, i);
-    updateShiftRegister();
-    delay(tDelay);
+    alarmTimeRemaining = alarmTimeRemaining - 1000;
+    displayTempTimer();
+    delay(1000);
+    if (alarmTimeRemaining == 0) 
+    {
+      alarmRinging = 1;
+      alarmActive = 0;
+      while (alarmStopped == 0) 
+      {
+        for (int thisNote = 0; thisNote < 8; thisNote++) 
+        {
+          tone(buzzer, melody[thisNote], duration);
+          delay(duration);
+          noTone(buzzer);
+        }
+        if (digitalRead(stopButton) == LOW) 
+        {
+          alarmStopped = 1;
+          alarmActive = 0;
+          delay(1000);
+        }
+      }
+    }
   }
-
-  // DHT void loop()
-  float temperature;
-  float humidity;
-
-  /* Measure temperature and humidity.  If the functions returns
-     true, then a measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
-  {
-    Serial.print( "T = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, H = " );
-    Serial.print( humidity, 1 );
-    Serial.println( "%" );
-  }
-if (alarmActive == 0)
-{
-  lcd.setCursor(0, 0);
-  lcd.print("Temp         C  ");
-  // Display Temperature in F
-  //lcd.print("Temp         F  ");
-  /* lcd.setCursor(6, 0);
-  // Display Temperature in C
-  lcd.print(temperature);
-  // Display Temperature in F
-  //lcd.print(tempF);
-  delay(500); */
+  
 }
- 
+// ----------------------------
 
-}
